473a11d2690479306150d6ad352b4eac9aab70e4
Bayron Najera <bayron.najeramar@ufl.edu>
2024-04-08 20:43:35 -0400

    Adding Unit tests for counter

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,31 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "../components/Counter";
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = document.querySelector('h2');
+  expect(counterMessage.textContent).toBe('Counter');
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const count = document.querySelector('p');
+  expect(count.textContent).toBe('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const incrementButton = document.querySelector('button');
+  fireEvent.click(incrementButton);
+  const count = document.querySelector('p');
+  expect(count.textContent).toBe('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const decrementButton = document.querySelectorAll('button')[1];
+  fireEvent.click(decrementButton);
+  const count = document.querySelector('p');
+  expect(count.textContent).toBe('-1');
 });
